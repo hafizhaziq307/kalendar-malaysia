fba36defab1e4497ba5daaabfd25e2e5b8eb84c7
Hafiz Haziq <hafizhaziq.dev@gmail.com>
2024-01-07 15:24:51 +0800

    update data 2024

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -2,73 +2,73 @@ export const data_months = [
   {
     title: "January",
     number: 1,
-    start: "sun",
+    start: "mon",
     end: 31,
   },
   {
     title: "February",
     number: 2,
-    start: "wed",
-    end: 28,
+    start: "thu",
+    end: 29,
   },
   {
     title: "March",
     number: 3,
-    start: "wed",
+    start: "fri",
     end: 31,
   },
   {
     title: "April",
     number: 4,
-    start: "sat",
+    start: "mon",
     end: 30,
   },
   {
     title: "May",
     number: 5,
-    start: "mon",
+    start: "wed",
     end: 31,
   },
   {
     title: "June",
     number: 6,
-    start: "thu",
+    start: "sat",
     end: 30,
   },
   {
     title: "July",
     number: 7,
-    start: "sat",
+    start: "mon",
     end: 31,
   },
   {
     title: "August",
     number: 8,
-    start: "tue",
+    start: "thu",
     end: 31,
   },
   {
     title: "September",
     number: 9,
-    start: "fri",
+    start: "sun",
     end: 30,
   },
   {
     title: "October",
     number: 10,
-    start: "sun",
+    start: "tue",
     end: 31,
   },
   {
     title: "November",
     number: 11,
-    start: "wed",
+    start: "fri",
     end: 30,
   },
   {
     title: "December",
     number: 12,
-    start: "fri",
+    start: "sun",
     end: 31,
   },
 ];
@@ -87,226 +87,222 @@ export const data_events = [
   {
     title: "tahun baru",
     image: "tahun baru.png",
-    date: "1/1/2023",
+    date: "1/1/2024",
   },
   {
     title: "keputeraan YDP negeri sembilan",
     image: "negeri sembilan.png",
-    date: "1/14/2023",
+    date: "1/14/2024",
   },
-  {
-    title: "tahun baru cina",
-    image: "tahun baru cina.png",
-    date: "1/22/2023",
-  },
-  {
-    title: "tahun baru cina",
-    image: "tahun baru cina.png",
-    date: "1/23/2023",
-  },
-
-  // TODO:
   {
     title: "thaipusam",
     image: "thaipusam.png",
-    date: "2/5/2023",
+    date: "1/25/2024",
   },
   {
     title: "israk & mikraj",
     image: "israk & miraj.png",
-    date: "2/18/2023",
+    date: "2/8/2024",
+  },
+  {
+    title: "tahun baru cina",
+    image: "tahun baru cina.png",
+    date: "2/10/2024",
+  },
+  {
+    title: "tahun baru cina",
+    image: "tahun baru cina.png",
+    date: "2/11/2024",
   },
   {
     title: "pertabalan sultan terengganu",
     image: "terengganu.png",
-    date: "3/4/2023",
-  },
-  {
-    title: "keputeraan sultan johor",
-    image: "johor.png",
-    date: "3/23/2023",
+    date: "3/4/2024",
   },
   {
     title: "awal ramadhan",
     image: "ramadhan.png",
-    date: "3/23/2023",
+    date: "3/12/2024",
+  },
+  {
+    title: "keputeraan sultan johor",
+    image: "johor.png",
+    date: "3/23/2024",
   },
   {
     title: "nuzul al-quran",
     image: "nuzul al-quran.png",
-    date: "4/8/2023",
+    date: "3/28/2024",
   },
-  {
+  { // tidak berubah
     title: "hari melaka bandaraya bersejarah",
     image: "melaka.png",
-    date: "4/15/2023",
-  },
-  // TODO: hari raya puasa
-  {
-    title: "hari raya puasa",
-    image: "hari raya puasa.png",
-    date: "4/22/2023",
+    date: "4/15/2024",
   },
   {
     title: "hari raya puasa",
     image: "hari raya puasa.png",
-    date: "4/23/2023",
+    date: "4/10/2024",
   },
   {
+    title: "hari raya puasa",
+    image: "hari raya puasa.png",
+    date: "4/11/2024",
+  },
+  { // tidak berubah
     title: "keputeraan sultan terengganu",
     image: "terengganu.png",
-    date: "4/26/2023",
+    date: "4/26/2024",
   },
-  {
+  { // tidak berubah
     title: "hari pekerja",
     image: "hari pekerja.png",
-    date: "5/1/2023",
-  },
-  {
-    title: "hari wesak",
-    image: "wesak.png",
-    date: "5/4/2023",
+    date: "5/1/2024",
   },
   {
     title: "hari ibu",
     image: "hari ibu.png",
-    date: "5/14/2023",
+    date: "5/12/2024",
   },
   {
+    title: "hari wesak",
+    image: "wesak.png",
+    date: "5/22/2024",
+  },
+  {  // tidak berubah
     title: "keputeraan raja perlis",
     image: "perlis.png",
-    date: "5/17/2023",
+    date: "5/17/2024",
   },
-  // TODO: hari hol pahang
-  {
+  {  // tidak berubah
     title: "pesta keamatan",
     image: "sabah.png",
-    date: "5/30/2023",
+    date: "5/30/2024",
   },
-  {
+  {  // tidak berubah
     title: "pesta keamatan",
     image: "sabah.png",
-    date: "5/31/2023",
+    date: "5/31/2024",
   },
-  {
+  {  // tidak berubah
     title: "hari gawai",
     image: "sarawak.png",
-    date: "6/1/2023",
+    date: "6/1/2024",
   },
-  {
+  {  // tidak berubah
     title: "hari gawai",
     image: "sarawak.png",
-    date: "6/2/2023",
+    date: "6/2/2024",
   },
   {
     title: "keputeraan YDP agong",
     image: "agong.png",
-    date: "6/5/2023",
-  },
-  {
-    title: "keputeraan sultan kedah",
-    image: "kedah.png",
-    date: "6/18/2023",
+    date: "6/3/2024",
   },
   {
     title: "hari arafah",
     image: "hari arafah.png",
-    date: "6/28/2023",
+    date: "6/16/2024",
   },
   {
     title: "hari raya haji",
     image: "hari raya haji.png",
-    date: "6/29/2023",
+    date: "6/17/2024",
   },
   {
     title: "hari raya haji",
     image: "hari raya haji.png",
-    date: "6/30/2023",
+    date: "6/18/2024",
   },
   {
-    title: "hari jadi YDP pulau pinang",
-    image: "pulau pinang.png",
-    date: "7/8/2023",
+    title: "keputeraan sultan kedah",
+    image: "kedah.png",
+    date: "6/30/2024",
   },
   {
     title: "awal muharram",
     image: "awal muharram.png",
-    date: "7/19/2023",
+    date: "7/7/2024",
   },
   {
+    title: "hari jadi YDP pulau pinang",
+    image: "pulau pinang.png",
+    date: "7/13/2024",
+  },
+  { // tidak berubah
     title: "hari sarawak",
     image: "sarawak.png",
-    date: "7/22/2023",
+    date: "7/22/2024",
   },
-  {
+  { // tidak berubah
     title: "keputeraan sultan pahang",
     image: "pahang.png",
-    date: "7/30/2023",
+    date: "7/30/2024",
   },
   {
     title: "hari hol almarhum sultan iskandar",
     image: "johor.png",
-    date: "8/23/2023",
+    date: "8/11/2024",
   },
-  {
+  { // tidak berubah
     title: "hari jadi YDP melaka",
     image: "melaka.png",
-    date: "8/24/2023",
+    date: "8/24/2024",
   },
-  {
+  { // tidak berubah
     title: "hari kebangsaan",
     image: "malaysia.png",
-    date: "8/31/2023",
+    date: "8/31/2024",
   },
 
-  {
+  { // tidak berubah
     title: "hari malaysia",
     image: "malaysia.png",
-    date: "9/16/2023",
+    date: "9/16/2024",
   },
   {
     title: "maulidur rasul",
     image: "maulidur rasul.png",
-    date: "9/28/2023",
+    date: "9/16/2024",
   },
   {
     title: "hari jadi YDP sabah",
     image: "sabah.png",
-    date: "10/7/2023",
+    date: "10/5/2024",
+  },
+  {
+    title: "keputeraan sultan kelantan",
+    image: "kelantan.png",
+    date: "9/29/2024",
+  },
+  {
+    title: "keputeraan sultan kelantan",
+    image: "kelantan.png",
+    date: "9/30/2024",
   },
   {
     title: "hari jadi YDP sarawak",
     image: "sarawak.png",
-    date: "10/14/2023",
+    date: "10/12/2024",
+  },
+    {
+    title: "hari deepavali",
+    image: "hari deepavali.png",
+    date: "10/31/2024",
   },
   {
     title: "keputeraan sultan perak",
     image: "perak.png",
-    date: "11/3/2023",
+    date: "11/1/2024",
   },
-  {
-    title: "keputeraan sultan kelantan",
-    image: "kelantan.png",
-    date: "11/11/2023",
-  },
-  {
-    title: "hari deepavali",
-    image: "hari deepavali.png",
-    date: "11/12/2023",
-  },
-  {
-    title: "keputeraan sultan kelantan",
-    image: "kelantan.png",
-    date: "11/12/2023",
-  },
-  {
+  { // tidak berubah
     title: "keputeraan sultan selangor",
     image: "selangor.png",
-    date: "12/11/2023",
+    date: "12/11/2024",
   },
-  {
+  { // tidak berubah
     title: "hari krismas",
     image: "hari krismas.png",
-    date: "12/25/2023",
+    date: "12/25/2024",
   },
 ];
